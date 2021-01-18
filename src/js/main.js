4b1d361b417be9d0273b4ea2d3bcdf320d7899b0
Purflix <39906866+Purflix@users.noreply.github.com>
2021-01-18 21:58:55 +0200

    Виправлення проблеми 100vh на телефонах

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // отримуємо ігрове поле з HTML
 const gameField = document.getElementById("game-field");
@@ -34,7 +34,8 @@ let timer;
 // Час, що пройшов при запуску таймера
 let timeLeft = 0;
 // Час анімації клітинок при виграші
-const animationTimeStart = 350, animationTimeEnd = 250;
+const animationTimeStart = 350,
+   animationTimeEnd = 250;
 
 // нормує задане число добавляючи нулі в кінець якщо це потрібно: 1.2 => 1,20, 5 => 5,00
 const normalizeTimeLeft = (valueStr) => {
@@ -54,10 +55,9 @@ const normalizeTimeLeft = (valueStr) => {
    return valueStr;
 };
 
-
 // створення ігрового поля
 const createCells = () => {
-   // створюємо всі клітки ігрового поля, крім пустої 
+   // створюємо всі клітки ігрового поля, крім пустої
    for (let i = 1; i < countCells; ++i) {
       // створюємо елемент-клітку
       const liCell = document.createElement("li");
@@ -79,12 +79,12 @@ const createCells = () => {
    liCell.dataset.index = countCells;
    liCell.style.flexBasis = `${100 / countRows}%`;
    gameField.appendChild(liCell);
-}
+};
 
 // видалення ігрового поля
 const removeCells = () => {
    gameField.innerHTML = ``;
-}
+};
 
 // зміна станів ігрових кліток
 const switchStates = (cellIndex1, cellIndex2) => {
@@ -93,16 +93,16 @@ const switchStates = (cellIndex1, cellIndex2) => {
    const cell2 = gameField.querySelector(`.game-field__cell:nth-child(${cellIndex2})`);
 
    // дістали значення цих кліток
-   const val1 = cell1.innerHTML, val2 = cell2.innerHTML;
+   const val1 = cell1.innerHTML,
+      val2 = cell2.innerHTML;
    // поміняли місцями їх значення
-   cell1.innerHTML = val2, cell2.innerHTML = val1;
-} 
+   (cell1.innerHTML = val2), (cell2.innerHTML = val1);
+};
 
 // перевірка на дотик
 const isAdjacentCell = (indexOfCell) => {
    // клітинка не належить полю
-   if (indexOfCell < 1 || indexOfCell > countCells)
-      return false;
+   if (indexOfCell < 1 || indexOfCell > countCells) return false;
 
    // чи має клітка сусідів відповідно згори, знизу, ліворуч, праворуч?
    let haveTop = true,
@@ -123,35 +123,34 @@ const isAdjacentCell = (indexOfCell) => {
    if (haveBottom && emptyCellIndex === indexOfCell + countRows) return true;
 
    return false;
-} 
-
+};
 
 // закриття вікна вибору розміру поля
 const closeGameFieldSize = () => {
    gameFieldSizeWrapper.classList.add("hidden");
-}
+};
 
 // відкриття вікна вибору розміру поля
 const openGameFieldSize = () => {
    gameFieldSizeWrapper.classList.remove("hidden");
-}
+};
 
 // відкриття гри
 const openGame = () => {
    const game = document.querySelector(".game-wrapper");
    game.classList.remove("hidden");
-}
+};
 
 // закриття гри
 const closeGame = () => {
    const game = document.querySelector(".game-wrapper");
    game.classList.add("hidden");
-}
+};
 
 // відкриття вікна перемоги
 const openGameWinModal = () => {
    gameWinModalWrapper.classList.remove("hidden");
-}
+};
 
 // закриття вікна перемоги
 const closeGameWinModal = () => {
@@ -161,7 +160,7 @@ const closeGameWinModal = () => {
 // зміна розміру поля
 const changeFieldSize = (size) => {
    gameField.dataset.countcells = size;
-}
+};
 
 // оновлення глобальних змінних після зміни розміру поля
 const updateChanges = () => {
@@ -169,24 +168,24 @@ const updateChanges = () => {
    countCells = gameField.dataset.countcells;
    countRows = +Math.sqrt(countCells);
    emptyCellIndex = +countCells;
-}
+};
 
 // обнулення кількості ходів
 const resetCountMoves = () => {
    countMoves.innerHTML = `-`;
    moves = 0;
-}
+};
 
 // оновлення кількості ходів
 const updateCountMoves = () => {
    moves++;
    countMoves.innerHTML = `${moves}`;
-}
+};
 
 // повертає випадкове число в інтервалі [1, 4]
 const getRandomNumberFrom1to4 = () => {
    return Math.ceil(Math.random() * 4);
-}
+};
 
 // повертає індекс клітинки по заданому напрямку, де {1 - top, 2 - right, 3 - bottom, 4 - left}
 const getIndexOfCellByDirection = (direction) => {
@@ -211,7 +210,7 @@ const getIndexOfCellByDirection = (direction) => {
    }
 
    return indexOfCell;
-}
+};
 
 // перемішування ігрового поля
 const gameShuffle = () => {
@@ -223,19 +222,19 @@ const gameShuffle = () => {
          emptyCellIndex = indexOfCell;
       }
    }
-}
+};
 
-// оновлення рекорду гри в екрані перемоги 
+// оновлення рекорду гри в екрані перемоги
 const updateGameWinInfo = () => {
    const gameWinScore = document.querySelector(".game-win-modal__score");
    const gameWinTime = document.querySelector(".game-win-modal__time");
    gameWinScore.innerHTML = `${moves}`;
-   gameWinTime.innerHTML  = `${normalizeTimeLeft(String(timeLeft))}`;
-}
+   gameWinTime.innerHTML = `${normalizeTimeLeft(String(timeLeft))}`;
+};
 
 // перевірка на виграш
 const isGameWin = () => {
-   const gameFieldCells = gameField.querySelectorAll('.game-field__cell');
+   const gameFieldCells = gameField.querySelectorAll(".game-field__cell");
    let currentIndex = 1;
    for (let i = 0; i < countCells - 1; ++i) {
       const index = +gameFieldCells[i].innerHTML;
@@ -246,16 +245,15 @@ const isGameWin = () => {
    }
 
    return true;
-}
-
+};
 
 // блокує нажимання на клітинки
 const blockClicks = () => {
-   const gameFieldCells = document.querySelectorAll('.game-field__cell');
+   const gameFieldCells = document.querySelectorAll(".game-field__cell");
    gameFieldCells.forEach((cell) => {
       cell.style.pointerEvents = "none";
    });
-}
+};
 
 // розблоковує нажимання на клітинки
 const unBlockClicks = () => {
@@ -270,7 +268,8 @@ const animationAfterWin = () => {
    // отримую матрицю елементи якого йдуть змійкою 123456789 => 123654789
    const arr = Array.from({ length: countCells }, (_, i) => i + 1);
    for (let i = 2; i <= countRows; i += 2) {
-      let left = (i - 1) * countRows, right = i * countRows - 1;
+      let left = (i - 1) * countRows,
+         right = i * countRows - 1;
       while (left < right) {
          const tmp = arr[left];
          arr[left] = arr[right];
@@ -294,20 +293,18 @@ const animationAfterWin = () => {
       }, time + animationTimeEnd);
       time += animationTimeStart;
    }
-   
-}
+};
 
-
-// оновлення часу гри на головному екрані 
+// оновлення часу гри на головному екрані
 const updateTimeOnGame = () => {
    countTimes.innerHTML = `${normalizeTimeLeft(String(timeLeft))}s`;
-}
+};
 
-// обнулення часу гри на головному екрані 
+// обнулення часу гри на головному екрані
 const resetTimeOnGame = () => {
    countTimes.innerHTML = `0,0s`;
    timeLeft = 0;
-}
+};
 
 // запуск таймера
 const startTimer = () => {
@@ -317,13 +314,13 @@ const startTimer = () => {
       //console.log(normalizeTimeLeft(String(timeLeft)));
       updateTimeOnGame();
    }, step);
-}
+};
 
 // очищення таймера
 const stopTimer = () => {
    clearInterval(timer);
    isFirstClick = true;
-}
+};
 
 // головна логіка гри
 function gameProcess(event) {
@@ -369,21 +366,20 @@ function gameProcess(event) {
 }
 
 // відслідковуємо клік по ігровому полю
-gameField.addEventListener('click', gameProcess);
+gameField.addEventListener("click", gameProcess);
 
 // відслідковуємо завершення свайпу по ігровому полю
-gameField.addEventListener("touchend", function(event) {
+gameField.addEventListener("touchend", function (event) {
    const location = event.changedTouches[0];
    // отримую елемент який є в позиції після відпускання пальця
    const elementAfter = document.elementFromPoint(location.clientX, location.clientY);
    // якщо задана клітинка над якою ми відпустили палець є пустою
-   if (elementAfter.innerHTML == "") 
-      gameProcess(event);
+   if (elementAfter.innerHTML == "") gameProcess(event);
 });
 
 // відслідковуємо клік по кнопці вибору розміру поля
 gameFieldSizeButtons.forEach((item) => {
-   item.addEventListener('click', function(event) {
+   item.addEventListener("click", function (event) {
       const gameSize = event.target.dataset.size;
 
       closeGameFieldSize();
@@ -400,7 +396,7 @@ gameFieldSizeButtons.forEach((item) => {
 });
 
 // відслідковуємо клік по кнопці перезапуску гри
-gameRestart.addEventListener('click', () => {
+gameRestart.addEventListener("click", () => {
    resetCountMoves();
    resetTimeOnGame();
    stopTimer();
@@ -408,7 +404,7 @@ gameRestart.addEventListener('click', () => {
 });
 
 // відслідковуємо клік по кнопці переходу до меню
-gameMenu.addEventListener('click', () => {
+gameMenu.addEventListener("click", () => {
    resetCountMoves();
    resetTimeOnGame();
    stopTimer();
@@ -419,7 +415,7 @@ gameMenu.addEventListener('click', () => {
 });
 
 // закриття вікна перемоги і запуск нової гри.
-gameWinCloseButton.addEventListener('click', () => {
+gameWinCloseButton.addEventListener("click", () => {
    closeGameWinModal();
    resetCountMoves();
    resetTimeOnGame();
